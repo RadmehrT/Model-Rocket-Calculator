d760b4085c63bf7e010f76f7b156e027029fc73d
RIGGZHyper <radmehrtp@gmail.com>
2025-06-16 19:55:44 -0700

    Refactor equation functions to use correct form of equation

--- rocketThing.py
+++ rocketThing.py
@@ -7,15 +7,17 @@ g = 9.81  # gravitational acceleration (m/s^2)
 air_density = 1.225  # air density at sea level (kg/m^3)
 drag_coefficient = 0.75  # drag coefficient (assumed)
 rocket_mass = 0.1448  # mass of the rocket (kg)
+propellant_mass = 45.6 / 1000 # mass of propellant (kg)
 engine_thrust = 12.5  # updated maximum thrust of the engine (N)
 burn_time = 1.6  # engine burn time (seconds)
 total_impulse = 20  # total impulse (Ns)
 diameter = 0.041656  # diameter of the rocket (m)
 
-def json_creator(rocket_mass, engine_thrust, burn_time, total_impulse, diameter, drag_coefficient, air_density, g):
+def json_creator(rocket_mass, propellant_mass, engine_thrust, burn_time, total_impulse, diameter, drag_coefficient, air_density, g):
     
     data = {
         "rocket_mass": rocket_mass,
+        "propellant mass": propellant_mass,
         "engine_thrust": engine_thrust,
         "burn_time": burn_time,
         "total_impulse": total_impulse,
@@ -29,16 +31,15 @@ def json_creator(rocket_mass, engine_thrust, burn_time, total_impulse, diameter,
     with open("rocket_stats.json", "w") as json_file:
         json.dump(data, json_file, indent=2)
     
-json_creator(rocket_mass, engine_thrust, burn_time, total_impulse, diameter, drag_coefficient, air_density, g)
+json_creator(rocket_mass, propellant_mass, engine_thrust, burn_time, total_impulse, diameter, drag_coefficient, air_density, g)
 
 def json_reader(filename):
     with open(filename, "r") as file:
         loaded_file = json.load(file)
     return loaded_file
 
-temp = json_reader("rocket_stats.json")
 
-print(temp.get("rocket_mass"))
+
 
 #Fehskens-Malewicki equations
 def burnout_velocity_Fehskens_Malewicki_equations(mass, thrust, burn_time, diameter, g, air_density, Cd):
@@ -57,7 +58,13 @@ def burnout_velocity_Fehskens_Malewicki_equations(mass, thrust, burn_time, diame
    
     # Burnout Velocity
     Vb = math.sqrt( (thrust - (mass * g)) / k) * math.tanh( (burn_time / mass) * math.sqrt(k * (thrust - (mass * g))))
-    print("burnout velolicty: ", Vb, "m/s")
+    print("burnout velocity: ", Vb, "m/s")
+    
+    return Vb
+   
+   
+   
+   
    
 def burnout_altitude_Fehskens_Malewicki_equations(mass, thrust, burn_time, diameter, g, air_density, Cd):
     # Drag Coefficient
@@ -68,16 +75,54 @@ def burnout_altitude_Fehskens_Malewicki_equations(mass, thrust, burn_time, diame
     
     mass = averageMass
     
+    #burnout Velocity
+    Vb = burnout_velocity_Fehskens_Malewicki_equations(rocket_mass, thrust, burn_time, diameter, g, air_density, drag_coefficient)
+    
     # Frontal Area
     A = math.pi * (diameter / 2) ** 2
    
     k =  0.5 * air_density * 0.75 * A
    
     # Burnout Altitude
-    Yb = (mass / k) * math.log(math.cosh(burn_time / mass) * math.sqrt(k * (thrust - (mass * g))))
+    Yb = -(mass / (2 * k)) * math.log((thrust - mass*g - k * Vb**2) / (thrust - mass*g))
+
     print("Burnout Altitude: ", Yb, "m")
+    
+    return Yb
    
+   
+   
+def coast_altitude_Fehskens_Malewicki_equations(rocket_mass, propellant_mass, thrust, burn_time, diameter, g, air_density, Cd):
+    # Drag Coefficient
+    if Cd == None:
+        Cd = 0.75
+        
+    # burnout mass
+    Mb = rocket_mass - propellant_mass
+    
+    #burnout Velocity
+    Vb = burnout_velocity_Fehskens_Malewicki_equations(rocket_mass, thrust, burn_time, diameter, g, air_density, drag_coefficient)
+    
+    # Frontal Area
+    A = math.pi * (diameter / 2) ** 2
+   
+    k =  0.5 * air_density * 0.75 * A
+   
+    # Coast Altitude
+    Yc = (Mb / (2 * k)) * math.log((Mb * g + k * Vb**2) / (Mb * g))
+        
+    print("Coast Altitude: ", Yc, "m")
+    
+    return Yc
 
 burnout_velocity_Fehskens_Malewicki_equations(rocket_mass, engine_thrust, burn_time, diameter, g, air_density, drag_coefficient)
 
-burnout_altitude_Fehskens_Malewicki_equations(rocket_mass, engine_thrust, burn_time, diameter, g, air_density, drag_coefficient)
+Yb = burnout_altitude_Fehskens_Malewicki_equations(rocket_mass, engine_thrust, burn_time, diameter, g, air_density, drag_coefficient)
+
+Yc = coast_altitude_Fehskens_Malewicki_equations(rocket_mass, propellant_mass, engine_thrust, burn_time, diameter, g, air_density, drag_coefficient)
+
+print("Apogee: ", Yb + Yc)
+
+temp = json_reader("rocket_stats.json")
+
+print(temp.get("rocket_mass"))
